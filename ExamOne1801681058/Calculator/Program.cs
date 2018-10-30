40e4f64674dca646346dfa4220f21736011c7328
hrisnedqlkova <joanapal17@gmail.com>
2018-10-30 18:00:45 +0200

    Exam from week 5

---
+++ ExamOne1801681058/Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 10;
+			double b = 12.78;
+
+			string operator4e = Console.ReadLine();
+			switch (operator4e)
+			{
+				case "+":
+					double sum = a + b;
+					double withPercentage = sum + (sum * (10 / 100));
+				default:
+					break;
+			}
+		}
+	}
+}
---
+++ ExamOne1801681058/RectangleArea/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RectangleArea
+{
+	class Program
+	{
+		static void Main()
+		{
+			//Rectangle Area: A (area) = L (length) x W (width)
+			Console.Write("Please write the length of your rectangle: ");
+			decimal lengthSide = decimal.Parse(Console.ReadLine());
+			Console.Write("Please write the width of your rectangle: ");
+			decimal widthSide = decimal.Parse(Console.ReadLine());
+
+			decimal area = lengthSide * widthSide;
+			Console.WriteLine("The area of your rectangle is: {0}", area);
+		}
+
+     }
+
+}
+    
+
